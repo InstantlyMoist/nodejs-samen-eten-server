e56e1a71b8f02e3eb36ba62abc30c83f6620456f
InstantlyMoist <kyllian007@gmail.com>
2020-04-16 22:05:41 +0200

    Added email detection

--- routes/login.js
+++ routes/login.js
@@ -11,6 +11,8 @@ router.post('/', (req, res) => {
     if (receivedUser.firstName == null) res.end("MISSING_FIELD");
     if (receivedUser.lastName == null) res.end("MISSING_FIELD");
     if (receivedUser.email == null) res.end("MISSING_FIELD");
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(receivedUser.email).toLowerCase())) res.end("INVALID_EMAIL");
     if (receivedUser.studentNr == null) res.end("MISSING_FIELD");
     if (receivedUser.password == null) res.end("MISSING_FIELD");
     res.end("ok");
